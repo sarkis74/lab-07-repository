35cb3ad4c6ef52fed688d52c42a0dbde27713c23
sarkis7412 <sarkis_aghazarian@yahoo.com>
2018-12-08 16:43:48 -0800

    latest

--- server.js
+++ server.js
@@ -4,10 +4,12 @@
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+
 //Load env vars
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+console.log(PORT)
 
 //app
 const app = express();
@@ -18,56 +20,62 @@ app.use(cors());
 
 app.get('/location', getLocation)
 
-app.get('/weather', getWeather)
+// app.get('/weather', getWeather)
 
 
 //Handlers
 function getLocation(request, response){
-  return searchToLatLong(request.query.data) //Lynnwood,WA
-  .then(locationData => {
+  return searchToLatLong(request.query.data).then(locationData => {
     response.send(locationData); //Promise: when superagent makes a promise to the frontend
   })
+  .catch(err => err);
  
 }
 
-function getWeather(req, res){
-  const weatherData = searchForWeather(req.query);
-  res.send(weatherData);
-}
+// function getWeather(req, res){
+//   const weatherData = searchForWeather(req.query.data);
+//   res.send(weatherData);
+// }
 
-//Constructor
-function Weather(weather) {
-  this.forecast = weather.summary;
-  this.time = new Date(weather.time * 1000).toDateString();
-}
+// //Constructor
+// function Weather(weather) {
+//   this.forecast = weather.summary;
+//   this.time = new Date(weather.time * 1000).toDateString();
+// }
 
-function Location(location){
+function Location(location, response){
   this.formatted_query = location.formatted_address;
+  console.log(this.formatted_query)
   this.latitude = location.geometry.location.lat;
   this.longitude = location.geometry.location.lng;
 }
 
 //Search for Resources
 function searchToLatLong(query){
-  return superagent.get(`https://maps.googleapis.com/api/geocode/json?address=$key=${process.env.GEOCODE_API_KEY}`)
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+ 
+  return superagent.get(url)
   .then(geoData => {
-  const location = new Location(geoData.results[0]);
+   
+    console.log(geoData.body.results[0], 'GEODATA RESULTS')
+  const location = new Location(geoData.body.results);
+  console.log(location)
   return location;
   }) 
-  .catch(err => console.log(err));
+  .catch(err => err);
 }
 
-function searchForWeather(query){
-  const weatherData = require('./data/darksky.json');
-  let dailyForecast = [];
-  weatherData.daily.data.map(weather => dailyForecast.push(new Weather(weather)));
-  return dailyForecast;
+// function searchForWeather(query){
+//   const weatherData = require('./data/darksky.json');
+//   let dailyForecast = [];
+//   weatherData.daily.data.map(weather => dailyForecast.push(new Weather(weather)));
+//   return dailyForecast;
   
-}
+// }
 
 //Give error message if incorrect
 app.get('/*', function(req, res){
-  res.status(404).send('you are in the wrong place');
+  // res.status(404).send('you are in the wrong place');
 })
 
 //THIS must be at bottom of code!!!
